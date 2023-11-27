f087dc38111c762b674b02abad0c912ca7e30129
Zuzana Petrlikova <zuzana.petrlikova@outlook.com>
2023-11-27 18:16:08 +0100

    Modified 3.Question according to comments

--- 3. Question.sql
+++ 3. Question.sql
@@ -5,28 +5,6 @@
 
  */
 
-SELECT DISTINCT  
-		pf.food_name,
-		pf.food_category_code,
-		pf.year,
-		subquery.year + 1 AS next_year,
-		round( ( pf.food_value - subquery.food_value ) / subquery.food_value * 100, 2 ) as percentage_food_value_growth
-FROM t_zuzana_petrlikova_project_sql_primary_final AS pf
-JOIN t_zuzana_petrlikova_project_sql_primary_final AS subquery
-	ON pf.food_name = subquery.food_name
-	AND pf.YEAR = subquery.year + 1
-ORDER BY percentage_food_value_growth ASC; 
-
-/*
-  Odpovìï
-  
-	Nejnižší procentuální nárùst byl zaznamenán v roce 2007 u potraviny: Rajská jablka èervená kulatá. Cena poklesla o 31.03%.
-
- */
-
-
- -- Mezi lety 2006-2018
-
 SELECT
     food_category_code,
     food_name,
@@ -44,4 +22,27 @@ ORDER BY percentage_change;
 	Mezi lety 2006 a 2018 nejpomaleji zdražuje: cukr krystalový(212,101), u nìhož došlo ke snížení ceny o 27.27%.
 	
  */
+
+ -- V jednotlivých letech
+
+SELECT DISTINCT  
+		pf.food_name,
+		pf.food_category_code,
+		pf.year,
+		subquery.year + 1 AS next_year,
+		round( ( pf.food_value - subquery.food_value ) / subquery.food_value * 100, 2 ) as percentage_food_value_growth
+FROM t_zuzana_petrlikova_project_sql_primary_final AS pf
+JOIN t_zuzana_petrlikova_project_sql_primary_final AS subquery
+	ON pf.food_name = subquery.food_name
+	AND pf.YEAR = subquery.year + 1
+ORDER BY percentage_food_value_growth ASC; 
+
+/*
+  Odpovìï
+  
+	Nejnižší percentuální nárùst byl zaznamenán v roce 2007 u potraviny: Rajská jablka èervená kulatá. Cena poklesla o 31.03%.
+
+ */
+
+
 
